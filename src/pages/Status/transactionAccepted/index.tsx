25f09546f16e734530880fe11b3eb8d3ff57df67
valentimcanejo <valentim.canejo@hotmail.com>
2022-01-05 23:25:28 -0300

    Tela do componente transactionAccepted atualizada

--- src/pages/Status/transactionAccepted/index.tsx
+++ src/pages/Status/transactionAccepted/index.tsx
@@ -4,11 +4,44 @@ import {
   CardMedia,
   Grid,
   Paper,
+  Box,
   Typography,
 } from "@mui/material";
 import React, { useState } from "react";
 import "./style.css";
 import Check from "../../../assets/check.png";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles(() => ({
+  paper: {
+    padding: 30,
+    textAlign: "center",
+    marginLeft: "15px",
+  },
+  buttonEndAdornment: {
+    fontWeight: "bold",
+    color: "rgba(230,198,139,255)",
+  },
+  buttonA: {
+    marginTop: "10px",
+    color: "rgba(244,203,98,255)",
+    borderColor: "rgba(244,203,98,255)",
+  },
+  buttonB: {
+    color: "rgba(192,192,192,255)",
+  },
+  buttonC: {
+    marginTop: "10px",
+    color: "black",
+    backgroundColor: "rgba(244,203,98,255)",
+  },
+  grid: {
+    minHeight: "90vh",
+  },
+  openButton: {
+    alignItems: "right",
+  },
+}));
 
 export default function TransactionAccepted({ amount, info }: any) {
   const [fromUser, setFromUser] = useState("");
@@ -16,7 +49,7 @@ export default function TransactionAccepted({ amount, info }: any) {
   const [show, setShow] = useState(0);
   const [hash, setHash] = useState("");
   const [ether, setEther] = useState("");
-
+  const classes = useStyles();
   function onSubmit() {
     setShow(1);
     setFromUser(info[0].from);
@@ -30,96 +63,116 @@ export default function TransactionAccepted({ amount, info }: any) {
   }
 
   return (
-    <div>
+    <Box>
       {show === 0 ? (
-        <div className="sending">
-          <Grid container spacing={0} direction="row">
+        <Box>
+          <Grid
+            className={classes.grid}
+            container
+            justifyContent="center"
+            alignItems="center"
+            spacing={0}
+            direction="column"
+          >
             <Grid item>
-              <Grid
-                container
-                direction="column"
-                spacing={2}
-                className="wallet-form"
+              <Paper
+                className={classes.paper}
+                variant="elevation"
+                elevation={2}
               >
-                <Paper
-                  variant="elevation"
-                  elevation={2}
-                  className="wallet-background"
-                >
-                  <Grid className="circle-grid" item>
-                    <CardMedia
-                      style={{ height: 50, padding: "37%" }}
-                      image={Check}
-                    ></CardMedia>
-                  </Grid>
+                <CardMedia
+                  style={{ height: 50, padding: "37%" }}
+                  image={Check}
+                ></CardMedia>
+                <Grid container direction="column" spacing={2}>
                   <Grid item>
-                    <Typography
-                      className="typography"
-                      component="h1"
-                      variant="h5"
-                    >
+                    <Typography component="h1" variant="h5">
                       Transaction Confirmed
                     </Typography>
                   </Grid>
-                  <Typography
-                    color="GrayText"
-                    variant="caption"
-                    display="block"
-                  >
-                    Your transaction was confirmed successfully
-                  </Typography>
-                  <Grid className="tryAgain" item>
+                  <Grid item>
+                    <Typography
+                      color="GrayText"
+                      variant="caption"
+                      display="block"
+                    >
+                      Your transaction was confirmed successfully
+                    </Typography>
+                  </Grid>
+                  <Grid item>
                     <Button
                       onClick={onSubmit}
-                      style={{
-                        marginTop: "10px",
-                        color: "rgba(244,203,98,255)",
-                        borderColor: "rgba(244,203,98,255)",
-                      }}
-                      className="tryAgain-button"
+                      className={classes.buttonA}
                       variant="outlined"
                     >
                       VIEW TRANSACTION
                     </Button>
-
+                  </Grid>
+                  <Grid item>
                     <Button
                       onClick={refreshPage}
-                      style={{ color: "rgba(192,192,192,255)" }}
-                      className="close-buttom"
+                      className={classes.buttonB}
                       variant="text"
                     >
                       CLOSE
                     </Button>
                   </Grid>
-                </Paper>
-              </Grid>
+                </Grid>
+              </Paper>
             </Grid>
           </Grid>
-        </div>
+        </Box>
       ) : (
-        <Alert className="alert" severity="info">
-          <Typography color="GrayText" display="block">
-            Transaction's Information:
-          </Typography>
-          <b>From:</b> <Typography display="block">{fromUser}</Typography>
-          <b>To:</b> <Typography display="block">{toUser}</Typography>
-          <b>Hash:</b>{" "}
-          <Typography display="block">{hash.toString()}</Typography>
-          <b>Amount:</b> <Typography display="block">{ether}</Typography>
-          <Button
-            onClick={refreshPage}
-            className="tryAgain-button"
-            variant="contained"
-            style={{
-              marginTop: "10px",
-              color: "black",
-              backgroundColor: "rgba(244,203,98,255)",
-            }}
-          >
-            BACK
-          </Button>
-        </Alert>
+        <Grid
+          container
+          className={classes.grid}
+          justifyContent="center"
+          alignItems="center"
+          spacing={0}
+        >
+          <Grid item>
+            <Alert severity="info">
+              <Grid container spacing={4} direction="column">
+                <Grid item>
+                  <Typography color="GrayText" display="block">
+                    Transaction's Information:
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography display="block">
+                    <Typography>From:</Typography> {fromUser}
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography display="block">
+                    <Typography>To:</Typography> {toUser}
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography display="block">
+                    <Typography>Hash:</Typography> {hash.toString()}
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography display="block">
+                    <Typography>Amount:</Typography> {ether}
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Button
+                    onClick={refreshPage}
+                    variant="contained"
+                    fullWidth
+                    className={classes.buttonC}
+                  >
+                    BACK
+                  </Button>
+                </Grid>
+              </Grid>
+            </Alert>
+          </Grid>
+        </Grid>
       )}
-    </div>
+    </Box>
   );
 }
